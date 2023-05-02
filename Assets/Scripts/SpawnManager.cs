f74c56a275fc61625760507f10817b38d4b63cd8
Npiccio1 <124706466+Npiccio1@users.noreply.github.com>
2023-05-02 11:56:25 -0400

    Added Models to player/missile

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] enemyPrefabs;
     private float spawnRangeX = 5;
     private float spawnPosZ = 5;
+    public float interval;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("SpawnEnemy");
     }
 
     // Update is called once per frame
@@ -20,7 +21,15 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             //randomly generate enemy index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX , spawnRangeX) + transform.position.x, 0, spawnPosZ + transform.position.z);
+            
+        }
+    }
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX) + transform.position.x, 0, spawnPosZ + transform.position.z);
             int enemyIndex = Random.Range(0, enemyPrefabs.Length);
             Instantiate(enemyPrefabs[enemyIndex], spawnPos,
                 enemyPrefabs[enemyIndex].transform.rotation);
